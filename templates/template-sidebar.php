9e5ab263840466d514a82d34fe90853898623f2e
JoeChindemi <joechindemi@gmail.com>
2024-03-21 23:53:16 -0400

    Final Changes

--- templates/template-sidebar.php
+++ templates/template-sidebar.php
@@ -1,12 +1,36 @@
 <?php
 
-/* Template Name: Page with Sidebar */
+/* Template Name: Sidebar template */
 
 get_header(); ?>
-    <h1>
-        <?php echo get_the_title(); ?>
-    </hi>
+   <div class="title_container" style="background-image: linear-gradient(rgba(0, 0, 0, .7), rgba(0, 0, 0, .2)), url('<?php echo get_the_post_thumbnail_url(); ?>');">
+        <h1 class = "title">
+            <?php echo get_the_title(); ?>
+        </h1>
+    </div>
 
-    <p>This is a custom template page with a sidebar.</p>
+    <?php if (has_excerpt()): ?>
+        <div class="thumbnail-excerpt">
+            <?php echo get_the_excerpt(); ?>
+        </div>
+
+        <div class = "container">
+            <div class = "content">
+                <?php endif; ?>
+                    <?php
+                    echo get_the_content();
+                ?>
+            </div>
+
+        <div class= "sidebar">
+            <div class = "wrapper">
+                <?php
+                if (is_active_sidebar('main_sidebar')) :
+                    dynamic_sidebar('main_sidebar');
+                endif;
+                ?>
+            </div>
+        </div>
+    </div>
 
 <?php get_footer(); ?>
